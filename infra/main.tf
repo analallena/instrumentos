c36e24782c407b3d139d1d16c6a767a5ee9f9c75
Ana Lallena Arquillo <lallena.ana@gmail.com>
2024-12-30 21:01:34 +0100

    Store tf-state in S3

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket                  = "tf-state-instruments"
+    key                     = "terraform-instruments-project"
+    region                  = "eu-west-1"
+  }
+}
+
 provider "aws" {
   region = "eu-west-1"
 }
